52cd43632bd0886b944cffae95039b20d12f8a77
felipe.shiba <felipe.shiba@ifood.com.br>
2019-08-08 23:58:44 -0300

    changed map value to provider so it can  be lazily created

--- navigation/src/main/java/com/felipeshiba/navigation/navigation/NavigationHelper.kt
+++ navigation/src/main/java/com/felipeshiba/navigation/navigation/NavigationHelper.kt
@@ -3,13 +3,13 @@ package com.felipeshiba.navigation.navigation
 import androidx.fragment.app.Fragment
 import javax.inject.Provider
 
-typealias ActionResolverMap = @JvmSuppressWildcards Map<Class<out ActionKey>, ActionResolver<*>>
+typealias ActionResolverMap = @JvmSuppressWildcards Map<Class<out ActionKey>, Provider<ActionResolver<*>>>
 
 open class Navigator(
     private val actionResolver: ActionResolverMap
 ) {
     open fun createFragment(key: ActionKey): Fragment {
-        val resolver = actionResolver[key::class.java] as ActionResolver<ActionKey>?
+        val resolver = actionResolver[key::class.java]?.get() as ActionResolver<ActionKey>?
 
         return resolver?.getAction(key)
             ?: throw IllegalStateException("Fragment does not exist in the graph")
